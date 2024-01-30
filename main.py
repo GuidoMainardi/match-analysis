027587c4aa2ee223fd0164289436bafa02edf461
Guido Mainardi <guidoluis.gm@gmail.com>
2024-01-29 21:38:10 -0300

    Add prepare base model pipeline

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from src.MatchAnalysis import logger
 from src.MatchAnalysis.pipeline.stage_01_data_ingestion import DatatIngestionTrainingPieline
+from src.MatchAnalysis.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = 'Data Ingestion'
 try:
     logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
     obj = DatatIngestionTrainingPieline()
+    obj.main()
+    logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<<')
+except Exception as e:
+    logger.error(e)
+    raise e
+
+
+
+
+
+STAGE_NAME = 'Prepare base model'
+try:
+    logger.info(f'*'*20)
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
+    obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<<')
 except Exception as e:
--- src/MatchAnalysis/config/configuration.py
+++ src/MatchAnalysis/config/configuration.py
@@ -1,6 +1,9 @@
 from src.MatchAnalysis.constants import *
 from src.MatchAnalysis.utils.common import read_yaml, create_directories
+
 from src.MatchAnalysis.entity.config_entity import DataIngestionConfig
+from src.MatchAnalysis.entity.config_entity import PrepareBaseModelConfig
+
 
 class ConfigurationManager:
     def __init__(
@@ -28,3 +31,19 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+
+
+        config = self.config.prepare_base_model
+
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir = Path(config.root_dir),
+            base_model_path = Path(config.base_model_path),
+            updated_base_model_path = Path(config.updated_base_model_path),
+            params_n_jobs = self.params.N_JOBS
+        )
+
+        return prepare_base_model_config
